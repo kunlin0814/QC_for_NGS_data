bec9a5e3b85a60e905ba9b1152ae122229a581b9
kunlin0814 <abc730814@gmail.com>
2020-02-06 23:20:03 -0500

    Update Randomness.py

--- Randomness.py
+++ Randomness.py
@@ -45,7 +45,7 @@ skewness = skew(np.array(original_list))
 
 output = open(file_name+'_randomness_summary.txt','w')
 
-output.write(file_name+'\t'+str(average)+'\t'+str(std)+'\t'+str(skewness))
+output.write(file_name+'\t'+str(average)+'\t'+str(std)+'\t'+str(skewness)+'\n')
 output.close()
 
 
