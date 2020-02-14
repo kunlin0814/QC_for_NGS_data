629fed5d471c3f7cfef1c565c76e7d4f06832c3b
Kun-Lin Ho <kun-linho@Kun-Lins-MacBook-Pro.local>
2020-02-13 21:13:19 -0500

    add sum of square of error

--- Distribution.R
+++ Distribution.R
@@ -1,44 +1,63 @@
-library(ggplot2)
-library(readxl)
-library(dplyr)
-
-
-
-sample <- read.table("C:\\Users\\abc73_000\\Desktop\\Pan_cancer_mapping_result\\Randomness\\Distribution\\Mammary\\SRR7780805_DepthofCoverage_Distribution.txt",
-                     sep ="",header = F, stringsAsFactors = F)
-
-sample <- sample[-2,]
-final <- sample[0:600,]
-colnames(sample)=c('Frequency',"Position")
-#png("/Users/kun-linho/Desktop/Pan_cancer_mapping_result/Randomness/Distribution/Mammary/Plot/600_cut_distribution.png",width=3000,height=2400,res=300)  
-freq = as.numeric(sample$Frequency)
-pos = as.numeric(sample$Position)
-ggplot()+ 
-  geom_line(data = sample,aes(x=pos, y=freq),stat="identity")+
-  ylim(0,200000)
-  
-dev.off()
-
-DrawDistribution <- function(file_location, xupper, yupper){
+DrawDistribution <- function(file_location, withlimit, xupper,file_name){
+  library(ggplot2)
+  library(readxl)
+  library(dplyr)
+  title <- strsplit(file_name,split ="_")[[1]][1]
+  if (withlimit){
   sample <- read.table(file_location, sep =" ",header =F,
                        stringsAsFactors = F)
   sample <- sample[-2, ]
   final <- sample[0:xupper, ]
+  yupper = as.numeric(max(final[1]))
   colnames(final)=c('Frequency',"Position")
   freq = as.numeric(final$Frequency)
   pos = as.numeric(final$Position)
   p <- ggplot()+ 
     geom_line(data = final,aes(x=pos, y=freq),stat="identity")+
     ylim(0,yupper)+
-    ggtitle("Sequence Randomness")+
+    ggtitle(paste(title,"Sequence Randomness",sep="_"))+
     ylab("# of Position")+
-    xlab("Cover time")+
+    xlab("The frequency of being cover")+
     theme(axis.text=element_text(size=10),
           axis.title=element_text(size=14,face="bold"),
           plot.title = element_text(color = "black", size = 20, face = "bold"))
-  return(p)
-  
+  return(plot(p))
+  }
+  else{
+    sample <- read.table(file_location, sep =" ",header =F,
+                         stringsAsFactors = F)
+    sample <- sample[-2, ]
+    final <- sample
+    colnames(final)=c('Frequency',"Position")
+    freq = as.numeric(final$Frequency)
+    pos = as.numeric(final$Position)
+    p <- ggplot()+ 
+      geom_line(data = final,aes(x=pos, y=freq),stat="identity")+
+      ggtitle(paste(title,"Sequence Randomness",sep="_"))+
+      ylab("# of Position")+
+      xlab("Cover time")+
+      theme(axis.text=element_text(size=10),
+            axis.title=element_text(size=14,face="bold"),
+            plot.title = element_text(color = "black", size = 16, face = "bold"))
+    return(plot(p))
+    }
 }
 
-DrawDistribution("C:\\Users\\abc73_000\\Desktop\\Pan_cancer_mapping_result\\Randomness\\Distribution\\Mammary\\SRR7780805_DepthofCoverage_Distribution.txt",
-                 600,200000)
+
+
+# DrawDistribution("/Users/kun-linho/Desktop/Pan_cancer_mapping_result/Distribution/Mammary/Normal/SRR7780741_DepthofCoverage_Distribution.txt",
+#                  T,600)
+# 
+# dev.off()
+Df  <- "/Users/kun-linho/Desktop/Pan_cancer_mapping_result/Distribution/Lym/Lymphoma_Tumor_Sequence_distribution.pdf"
+pdf(file=Df, w=7, h=5)
+file_list <- read.table("/Users/kun-linho/Desktop/Pan_cancer_mapping_result/Distribution/Lym/Tumor/Tumor_list",
+                        sep = "", header = F, stringsAsFactors = F)
+for (i in as.vector(file_list$V1)){
+  
+DrawDistribution(paste("/Users/kun-linho/Desktop/Pan_cancer_mapping_result/Distribution/Lym/Tumor/",i,sep = ""),
+                   T, 600,i)
+  #print(paste("/Users/kun-linho/Desktop/Pan_cancer_mapping_result/Distribution/Mammary/Normal/",i,sep = ""))
+ 
+}
+dev.off()
--- Randomness.py
+++ Randomness.py
@@ -2,19 +2,29 @@
 """
 Created on Wed Feb  5 18:53:41 2020
 
+The script take the depth of coverage as input file 
+to identify the frequency of the coverage of depth for all of the base
+position for each sample 
+
+
 @author: abc73_000
 """
 
+## only look at the position to 600 base, account for 98.6% lines of total_lines
 import sys
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.stats import skew 
+from scipy.stats import poisson
+from sklearn.metrics import mean_squared_error
+from math import sqrt
 
-
-input_file = sys.argv[1]
-file_name = sys.argv[2]
+input_file = '/Users/kun-linho/Desktop/Pan_cancer_mapping_result/Distribution/Mammary/Normal/SRR7780741_DepthofCoverage_Distribution.txt'
+#sys.argv[1]
+file_name = 'SRR7780741'
+#sys.argv[2]
 with open (input_file,'r') as f:
     file = f.read()
 
@@ -25,7 +35,7 @@ table.remove('1 Total_Depth')
 pos_number = []
 freq_number =[]
 original_list=[]    
-sum = 0    
+Total_line = 0    
 line = 0
 for content in table:
     #content_list = makeOneSpace(con)
@@ -35,10 +45,27 @@ for content in table:
     pos_number.append(pos)
     freq_number.append(freq)
     total = freq*pos
-    sum += total
+    Total_line += total
     for i in range(freq):
         original_list.append(pos)
-          
+
+total_pos_arr = np.array(original_list)
+original_list.clear()
+
+#frequency_array = np.array(freq_number)/Total_line
+df = {'Frequency': np.array(freq_number), 'Position': np.array(pos_number)}    
+data_frame = pd.DataFrame(data=df )
+#poisson_number = int(data_frame.iloc[-1]["Position"])
+Total600line = sum(data_frame['Frequency'][0:601]*data_frame['Position'][0:601])
+event_arr = data_frame['Frequency'][0:601].values/Total600line
+mu = 100
+poisson_list=[]
+for i in range(601):
+    poisson_list.append(poisson.pmf(i,mu))
+
+rmse = sqrt(mean_squared_error(event_arr, np.array(poisson_list)))    
+sumOferror= sum(event_arr[0:601]-np.array(poisson_list)[0:601])**2
+   
 std = np.std(np.array(original_list))
 average = np.mean(np.array(original_list))    
 skewness = skew(np.array(original_list))
@@ -52,9 +79,8 @@ output.close()
 ## median= position (sum(freq)+1)/2 th position 
 ## Calcaulte Standard deviation ###
 ## sqrt(sum of (freq*(value - mean)**2)/sum of (freq)) 
-"""
-df = {'Frequency': np.log2(np.array(freq_number)), 'Position': np.array(pos_number)}    
-data_frame = pd.DataFrame(data=df )
+
+
 
 plt.figure(figsize=(16,9))
 sns.set(font_scale=3)
@@ -62,7 +88,7 @@ sns.lineplot(x ='Position', y = 'Frequency', data =df)
 #sns.distplot(freq_number, bins = len(freq_number),kde=False, axlabel= 'Frequency', color='orange')      
 
 #plt.close() 
-  
+""" 
 def makeOneSpace(String):
     content = String.split(' ')
     i = 0
