7d459e1c33490329e357f630d2a50ccbc3a9bf0c
kunlin0814 <abc730814@gmail.com>
2020-02-23 19:26:51 -0500

    compare sanger with us

--- Sanger_mutation.R
+++ Sanger_mutation.R
@@ -2,8 +2,8 @@ library(ggplot2)
 library(readxl)
 library(dplyr)
 
-Mut_table <- read_excel("/Users/kun-linho/Desktop/Pan_cancer_mapping_result/Mutation_rate/Sanger_melanoma.xlsx",sheet ='Sanger_data')
-our_data <- read_excel("/Users/kun-linho/Desktop/Pan_cancer_mapping_result/Mutation_rate/Sanger_melanoma.xlsx",sheet ='Our_data')
+Mut_table <- read_excel("J:\\Pan_cancer\\Pan_cancer_mapping_result\\Mutation_rate\\Sanger_melanoma.xlsx",sheet ='Sanger_data')
+our_data <- read_excel("J:\\Pan_cancer\\Pan_cancer_mapping_result\\Mutation_rate\\Sanger_melanoma.xlsx",sheet ='Our_data_new')
 # ele <- Mut_table$Sample
 # 
 # summary <- list()
@@ -24,42 +24,41 @@ our_data <- read_excel("/Users/kun-linho/Desktop/Pan_cancer_mapping_result/Mutat
 # new_total_summary <- total_summary[order(-total_summary$Mutation_rate),]
 # write.table(total_summary,file ="C:\\Users\\abc73_000\\Desktop\\Sanger_summary.txt" ,quote = F, sep ="\t", row.names = F)
 
-png("/Users/kun-linho/Desktop/Pan_cancer_mapping_result/sager_mutation.png",width=3000,height=2400,res=300)  
-ggplot(data=new_total_summary, aes(x= factor(new_total_summary$Sample,levels = new_total_summary$Sample), y= Mutation_rate))+
-  geom_bar(stat="identity", fill="steelblue")+
-  xlab("Samples")+
-  ylab("Mutation Number")+
-  theme_minimal()
-
-dev.off()
-png("/Users/kun-linho/Desktop/Mutation_rate/sanger_mutation_rate.png",width=3000,height=2400,res=300)  
-Mut_table <- Mut_table[order(-Mut_table$Mut_rate),]
-ggplot(data=Mut_table, aes(x= factor(Mut_table$Sample,levels = Mut_table$Sample), y= Mut_rate))+
-  geom_bar(stat="identity", fill="steelblue")+
-  xlab("Samples")+
-  ylab("Mutation Number")+
-  theme_minimal()
-
-dev.off()
-
-png("/Users/kun-linho/Desktop/Mutation_rate/sanger_mutation_rate.png",width=3000,height=2400,res=300)  
-sanger_sort_data <- sanger_sort_data[order(-sanger_sort_data),]
-ggplot(data=our_sort_data, aes(x= factor(our_sort_data$file_name,levels = our_sort_data$file_name), y= non_retro_PASS))+
-  geom_bar(stat="identity", fill="steelblue")+
-  xlab("Samples")
-  ylab("Mutation Number")+
-  theme_minimal()+
+# png("/Users/kun-linho/Desktop/Pan_cancer_mapping_result/sager_mutation.png",width=3000,height=2400,res=300)  
+# ggplot(data=new_total_summary, aes(x= factor(new_total_summary$Sample,levels = new_total_summary$Sample), y= Mutation_rate))+
+#   geom_bar(stat="identity", fill="steelblue")+
+#   xlab("Samples")+
+#   ylab("Mutation Number")+
+#   theme_minimal()
+# 
+# dev.off()
+# png("/Users/kun-linho/Desktop/Mutation_rate/sanger_mutation_rate.png",width=3000,height=2400,res=300)  
+# Mut_table <- Mut_table[order(-Mut_table$Mut_rate),]
+# ggplot(data=Mut_table, aes(x= factor(Mut_table$Sample,levels = Mut_table$Sample), y= Mut_rate))+
+#   geom_bar(stat="identity", fill="steelblue")+
+#   xlab("Samples")+
+#   ylab("Mutation Number")+
+#   theme_minimal()
+# 
+# dev.off()
+# 
+# png("/Users/kun-linho/Desktop/Mutation_rate/sanger_mutation_rate.png",width=3000,height=2400,res=300)  
+# sanger_sort_data <- sanger_sort_data[order(-sanger_sort_data),]
+# ggplot(data=our_sort_data, aes(x= factor(our_sort_data$file_name,levels = our_sort_data$file_name), y= non_retro_PASS))+
+#   geom_bar(stat="identity", fill="steelblue")+
+#   xlab("Samples")
+#   ylab("Mutation Number")+
+#   theme_minimal()+
   
 
 dev.off()
 
 both_table <-  cbind(Mut_table, our_data$non_retro_PASS,our_data$non_retro_mutation_rate  )
 colnames(both_table) <- c("Samples", "Sanger_mut", "our_callable","Sanger_mut_rate","Our_mut","Our_mut_rate")
-both_table <- both_table[order(both_table$Our_mut_rate),]
+#both_table <- both_table[order(both_table$Our_mut_rate),]
 png("/Users/kun-linho/Desktop/Pan_cancer_mapping_result/new-y-axis-Mutation_rate_comparison.png",width=3000,height=2400,res=300)  
-ggplot(data= both_table, aes(x = both_table$Our_mut_rate, y=both_table$Sanger_mut_rate))+
+ggplot(data= both_table, aes(x = Our_mut, y=Sanger_mut))+
   geom_point(shape = 1, size =4)+
-  ylim(0,max(both_table$Our_mut_rate))+
   geom_abline(intercept = 0, slope = 1, color="red", 
               linetype="dashed", size=1.5)+
 xlab("Our  Data")+
