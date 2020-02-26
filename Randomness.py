143c966e703e2006abf1a733bb310cc26347ca9a
kunlin0814 <abc730814@gmail.com>
2020-02-26 00:09:50 -0500

    Update Randomness.py

--- Randomness.py
+++ Randomness.py
@@ -23,11 +23,13 @@ from math import sqrt
 
 
 
-input_file ="/Users/kun-linho/Desktop/Pan_cancer_mapping_result/Distribution/Mammary/Normal/SRR7780741_DepthofCoverage_Distribution.txt"
+input_file =sys.argv[1]
+#'G:\\Pan_cancer\\Pan_cancer_mapping_result\\Distribution\\Mammary\\Normal\\SRR7780741_DepthofCoverage_Distribution.txt' 
+#"/Users/kun-linho/Desktop/Pan_cancer_mapping_result/Distribution/Mammary/Normal/SRR7780741_DepthofCoverage_Distribution.txt"
 #'G:\\Pan_cancer\\Pan_cancer_mapping_result\\Distribution\\Mammary\\Normal\\SRR7780741_DepthofCoverage_Distribution.txt' 
 #sys.argv[1]
 #"/Users/kun-linho/Desktop/Pan_cancer_mapping_result/Distribution/Mammary/Normal/SRR7780741_DepthofCoverage_Distribution.txt"
-file_name = 'SRR7780741'
+file_name = sys.argv[2]
 #sys.argv[2]
 #"SRR7780741"
 #sys.argv[2]
@@ -44,21 +46,28 @@ for i in range(len(file)):
     summary[pos]= int(freq)
     for j in range(freq):
         original_list.append(pos)
+        
 total_line = len(original_list)
 last_pos = int(list(summary.keys())[-1])
 
 for i in range(last_pos+1):
     if i not in summary.keys():
         summary[i]=0
+        
 order_summary={}
 for i in sorted(summary.keys()):
     order_summary[i]=summary[i]
 
-
 total_data = pd.DataFrame(order_summary.items())
-total_data.columns = [ 'Position', 'Frequency',]
+total_data.columns = [ 'Position', 'Frequency']
+
+
+
 
 freq_arr = total_data['Frequency'].values
+pos_arr = total_data['Position'].values
+
+
 total_array = np.array(original_list)
 
 average = np.mean(np.array(original_list))
@@ -66,29 +75,25 @@ std = np.std(np.array(original_list))
 mu = average
 
 prob_arr= freq_arr/total_line
-poisson_list=[]
+freq_list= list(freq_arr)
+
+poisson_fract_list=[]
 for i in range(last_pos+1):
     value = poisson.pmf(i,mu)
-    poisson_list.append(value)
-#poisson.pmf(i,mu) for i in range(last_pos+1)
+    poisson_fract_list.append(value)
+#[poisson.pmf(i,mu) for i in range(last_pos+1)]
 
-rmse = sqrt(mean_squared_error(prob_arr, np.array(poisson_list))) ## compare the proportion
-sumOfSqerror= sqrt(sum((prob_arr-np.array(poisson_list))**2)) ## rmse that didn't divide the N, vs proportion
+rmse = sqrt(mean_squared_error(prob_arr, np.array(poisson_fract_list))) ## compare the proportion
+sumOfSqerror= sqrt(sum((prob_arr-np.array(poisson_fract_list))**2)) ## rmse that didn't divide the N, vs proportion
 
 poisson_list_count =[]
+
 for i in range(last_pos+1):
     value = poisson.pmf(i,mu)*total_line
     poisson_list_count.append(value)
-    
-poisson_original_list=[]
-for i in range(len(poisson_list)+1):
-    value = int(poisson.pmf(i,mu)*total_line)
-    for j in range(value+1):
-        poisson_original_list.append(value)
-    
-#=[poisson.pmf(i,mu)*total_line for i in range(last_pos+1)]
+        
 
-rmse_count = sqrt(mean_squared_error(freq_arr, np.array(poisson_list)))     
+rmse_count = sqrt(mean_squared_error(freq_arr, np.array(poisson_list_count)))     
 sumOfSqerror_count= sqrt(sum((freq_arr-np.array(poisson_list_count))**2))
 
 output = open(file_name+'_randomness_summary.txt','w')
@@ -100,19 +105,23 @@ output.close()
 ## median= position (sum(freq)+1)/2 th position 
 ## Calcaulte Standard deviation ###
 ## sqrt(sum of (freq*(value - mean)**2)/sum of (freq)) 
-
-
+"""
+poisson_original_list=[]
+for i in range(last_pos+1):
+  value =int(poisson.pmf(i,mu)*total_line)
+  for j in range(value):
+      poisson_original_list.append(i)
 
 plt.figure(figsize=(16,9))
 sns.set(font_scale=3)
 #sns.lineplot(x ='Position', y = 'Frequency', data =total_data)
 #sns.distplot(total_data['Frequency'],hist=False, kde=True, axlabel= 'Frequency', color='orange')
-p = sns.distplot(np.array(poisson_original_list), kde=False, axlabel= 'Position', color='blue', bins=1000)  
+p = sns.distplot(np.array(poisson_original_list), kde=True, axlabel= 'Position', color='blue', bins=1000)  
 p.set_yscale('log')   # set into log scale 
 #p = p.map(plt.hist, "value", color="r", log=True)      
-sns.kdeplot(total_array, shade=True, bw=0.1);
+sns.kdeplot(np.array(poisson_original_list), shade=True);
 #plt.close() 
-"""
+
 def makeOneSpace(String):
     content = String.split(' ')
     i = 0
