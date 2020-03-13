6d2938fdba46f5b664511c084450ab8a329b3042
Kun-Lin Ho <kun-linho@MacBook-Pro.local>
2020-03-13 16:10:16 -0400

    update figure

--- QC_process.R
+++ QC_process.R
@@ -1,16 +1,19 @@
 library(ggplot2)
 library(readxl)
 library(dplyr)
+library(wesanderson)
+library(RColorBrewer)
 # glioma <- read_excel("/Volumes/Research_Data/Pan_cancer/Pan_cancer_mapping_result/Supplement_Figure1/Supp1_Data.xlsx",
 #                      sheet= "Sheet1")
 # new_glioma <- na.omit(glioma)
 # mean(new_glioma$`0...2`[!new_glioma$`0...2`==0])
 
-total_file <- read_excel("G:\\Pan_cancer\\Pan_cancer_mapping_result\\Supplement_Figure1\\V2Supp1_Data.xlsx",
+total_file <- read_excel("/Volumes/Research_Data/Pan_cancer/Pan_cancer_mapping_result/Supplement_Figure1/V2Supp1_Data.xlsx",
                          sheet ="Total")
-PAIR <- read_excel("G:\\Pan_cancer\\Pan_cancer_mapping_result\\Supplement_Figure1\\V2Supp1_Data.xlsx",
+PAIR <- read_excel("/Volumes/Research_Data/Pan_cancer/Pan_cancer_mapping_result/Supplement_Figure1/V2Supp1_Data.xlsx",
                    sheet ="PAIRS")
-
+callable <- read_excel("/Volumes/Research_Data/Pan_cancer/Pan_cancer_mapping_result/Supplement_Figure1/V2Supp1_Data.xlsx",
+                       sheet ="Callable_Bases")
 ###### Sequence read pairs ######
 
 filtered <- total_file %>% 
@@ -38,6 +41,13 @@ finalTable <- cbind(Cases,filtered,Exclude_Reason)
 
 write.table(finalTable,file="G:\\Pan_cancer\\Pan_cancer_mapping_result\\Exclude_process\\seq_lt_5M.txt",
             quote = F,sep = "\t",row.names = F)  
+###### fraction >30  ######
+filtered <- total_file %>% 
+  filter(!Total_pairs < 5000000 | Total_pairs==NaN)  %>% 
+  filter(!gt_30_fraction < 0.25 |gt_30_fraction ==NaN) %>% 
+  filter(gt_30_fraction < 0.25) %>% 
+  #filter(as.numeric(uniq_CDS_region_paris_rates) < 0.3|uniq_CDS_region_paris_rates==NaN) %>% 
+  select(ID, gt_30_fraction,Status,Cancer_Type)
 
 ############# CDS #############
 
@@ -99,40 +109,96 @@ write.table(finalTable,
             file="G:\\Pan_cancer\\Pan_cancer_mapping_result\\Exclude_process\\mean_coveragelt10.txt",
             quote = F,sep = "\t",row.names = F)  
 
+### find the run-number need to be excluded ####
+exclude <- read_excel("/Volumes/Research_Data/Pan_cancer/Pan_cancer_mapping_result/Supplement_Figure1/V2Supp1_Data.xlsx",
+                      sheet = "Total_excluded")
 
-  
-  # ggplot(aes(x=factor(Cancer_type,levels = c("Mammary_Cancer","Melanoma", "Osteosarcoma","Lymphoma","Unclassified")),
-  #                         y=as.numeric(uniq_CDS_region_paris_rates),fill=Status,color=Status)) + 
-  # geom_point(size=0.01,position = position_jitterdodge(jitter.width = 0.2)) +
-  # ylab("Mean of the coverage")+
-  # #labs(subtitle = "p<0.01")+
-  # #stat_compare_means(aes(group=gene),label="p.signif",symnum.args = symnumargs,label.y = c(2.1,2.1,2.1,2.1,3,3)) +
-  # #scale_y_log10(limits=c(0.001,1000),breaks=c(0.001,0.01,0.1,1,10,100,1000),labels=c(0,0.01,0.1,1,10,100,1000)) +
-  # theme(axis.line = element_line(colour = "black"),
-  #       #panel.grid.major = element_blank(),
-  #       #panel.grid.minor = element_blank(),
-  #       #legend.position = "none",
-  #       legend.title = element_blank(),
-  #       legend.key=element_blank(),
-  #       #legend.text =element_text(color = c("firebrick","black")),
-  #       legend.background = element_rect(fill = "transparent"),
-  #       panel.border = element_blank(),
-  #       axis.title.x = element_blank(),
-  #       #axis.ticks.x = element_blank(),
-  #       axis.title.y = element_text(colour="black",size=18,margin = margin(1,0,0,0)),
-  #       plot.margin = margin(1, 10, 4, 5),
-  #       text = element_text(colour="black",size=14),
-  #       axis.text.x = element_text(colour=c("black"),
-  #                                  size=14,angle=45,vjust=1,hjust = 0.9),
-  #       axis.text.y = element_text(colour="black",size=14),
-  #       panel.background = element_blank())+
-  # stat_summary(fun.y = median, fun.ymin = median, fun.ymax = median,position = "dodge",
-  #              geom = "crossbar",size=0.1, width = .8,colour = "black")+
-  # #scale_x_discrete(labels=panel2label)+
-  # scale_color_manual(values = c("firebrick","darkolivegreen"))+
-  # scale_fill_manual(values=c("firebrick","darkolivegreen"))+
-  # scale_shape_manual(values = 20)
+# Total_exclude_run <- PAIR %>% 
+#   filter(Cases %in% exclude$Cases)
+# 
+# write.table(Total_exclude_run,
+#             file="/Volumes/Research_Data/Pan_cancer/Pan_cancer_mapping_result/Exclude_process/Exclude_run_sample.txt",
+#             quote = F,sep = "\t",row.names = F)  
 
+plot_result <- png("/Volumes/Research_Data/Pan_cancer/Pan_cancer_mapping_result/callable_bases.png",width=3000,height=2400,res=400)  
+
+### change the color of the point ###
+callable %>% 
+  filter(!Sample_Name %in% exclude$Cases) %>%
+  ggplot(
+  aes(x=factor(Cancer_Type,levels = c("Mammary Cancer","Melanoma", "Osteosarcoma","Lymphoma","Unclassified")),
+       y=as.numeric( Callable_bases)))+
+  geom_point(size=3,aes(color=Cancer_Type),position = position_jitterdodge(jitter.width = 0.1))+
+  ylab("Callable bases")+
+  ylim(0, max(callable$Callable_bases))+
+  #labs(subtitle = "p<0.01")+
+  #stat_compare_means(aes(group=gene),label="p.signif",symnum.args = symnumargs,label.y = c(2.1,2.1,2.1,2.1,3,3)) +
+  #scale_y_log10(limits=c(0.001,1000),breaks=c(0.001,0.01,0.1,1,10,100,1000),labels=c(0,0.01,0.1,1,10,100,1000)) +
+  theme(axis.line = element_line(colour = "black"),
+        #panel.grid.major = element_blank(),
+        #panel.grid.minor = element_blank(),
+        legend.position = "none",
+        legend.title = element_blank(),
+        legend.key=element_blank(),
+        #legend.text =element_text(color = c("firebrick","black")),
+        legend.background = element_rect(fill = "transparent"),
+        panel.border = element_blank(),
+        axis.title.x = element_blank(),
+        #axis.ticks.x = element_blank(),
+        axis.title.y = element_text(colour="black",size=18,margin = margin(1,0,0,0)),
+        plot.margin = margin(1, 10, 4, 5),
+        text = element_text(colour="black",size=14),
+        axis.text.x = element_text(colour=c("black"),
+                                   size=14,angle=45,vjust=1,hjust = 0.9),
+        axis.text.y = element_text(colour="black",size=14),
+        panel.background = element_blank())+
+  stat_summary(fun.y = median, fun.ymin = median, fun.ymax = median,position = "dodge",
+               geom = "crossbar",size=0.1, width = .8,colour = "black")+
+  scale_color_brewer(palette="Dark2") ### for scatter plot
+  #scale_x_discrete(labels=panel2label)+
+  #scale_color_manual(values=wes_palette(n=5, name="Set3")) ## for scatter plot Wes Anderson color palettes
+  #scale_fill_manual(values=wes_palette(n=5, name="Royal2")) ## for box plot, bar plot Wes Anderson color palettes
+  #scale_shape_manual(values = 20)
+dev.off()
+
+
+plot_result <- png("/Volumes/Research_Data/Pan_cancer/Pan_cancer_mapping_result/RMSE_coverage.png",width=3000,height=2400,res=400)  
+
+total_file %>% 
+  filter(!ID %in% exclude$Normal) %>% 
+  filter(!ID %in%exclude$Tumor) %>% 
+  ggplot(aes(x=factor(Cancer_Type,levels = c("Mammary_Cancer","Melanoma", "Osteosarcoma","Lymphoma","Unclassified")),
+  y=as.numeric(RMSE),fill=Status,color=Status)) +
+  geom_point(size=0.01,position = position_jitterdodge(jitter.width = 0.2)) +
+  ylab("Coverage RMSE")+
+  #labs(subtitle = "p<0.01")+
+  #stat_compare_means(aes(group=gene),label="p.signif",symnum.args = symnumargs,label.y = c(2.1,2.1,2.1,2.1,3,3)) +
+  #scale_y_log10(limits=c(0.001,1000),breaks=c(0.001,0.01,0.1,1,10,100,1000),labels=c(0,0.01,0.1,1,10,100,1000)) +
+  theme(axis.line = element_line(colour = "black"),
+        #panel.grid.major = element_blank(),
+        #panel.grid.minor = element_blank(),
+        #legend.position = "none",
+        legend.title = element_blank(),
+        legend.key=element_blank(),
+        #legend.text =element_text(color = c("firebrick","black")),
+        legend.background = element_rect(fill = "transparent"),
+        panel.border = element_blank(),
+        axis.title.x = element_blank(),
+        #axis.ticks.x = element_blank(),
+        axis.title.y = element_text(colour="black",size=18,margin = margin(1,0,0,0)),
+        plot.margin = margin(1, 10, 4, 5),
+        text = element_text(colour="black",size=14),
+        axis.text.x = element_text(colour=c("black"),
+                                   size=14,angle=45,vjust=1,hjust = 0.9),
+        axis.text.y = element_text(colour="black",size=14),
+        panel.background = element_blank())+
+  stat_summary(fun.y = median, fun.ymin = median, fun.ymax = median,position = "dodge",
+               geom = "crossbar",size=0.1, width = .8,colour = "black")+
+  #scale_x_discrete(labels=panel2label)+
+  scale_color_manual(values = c("firebrick","darkolivegreen"))+
+  scale_fill_manual(values=c("firebrick","darkolivegreen"))+
+  scale_shape_manual(values = 20)
+dev.off()
 #### callable bases #####
 
 callable <- read_excel("/Volumes/Research_Data/Pan_cancer/Pan_cancer_mapping_result/Supplement_Figure1/V2Supp1_Data.xlsx",
@@ -140,7 +206,7 @@ callable <- read_excel("/Volumes/Research_Data/Pan_cancer/Pan_cancer_mapping_res
 callable <- na.omit(callable)
 
 filter_callable <- callable %>%
-  filter(Callable_bases<2000000|Callable_bases==NaN) 
+  filter(!Callable_bases<2000000|Callable_bases==NaN) 
 rep_time <- nrow(filter_callable)
 reason <- rep("callable_bases < 2M", rep_time)  
 final_filter_callable <- cbind(filter_callable,reason)
@@ -234,3 +300,7 @@ ggplot(data= filter_randomness,aes(x=factor(Cancer_type,levels = c("Mammary_Canc
   scale_shape_manual(values = 20)
 
 dev.off()
+
+
+
+
