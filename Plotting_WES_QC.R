d5259b74415eceefb344f2f7e4bf2619c98a9035
kunlin0814 <abc730814@gmail.com>
2020-03-19 12:23:48 -0400

    test multi cores

--- Plotting_WES_QC.R
+++ Plotting_WES_QC.R
@@ -15,7 +15,7 @@ exclude <- read_excel("G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer_mapping_res
 
 
 plot_result <- png("G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer_mapping_result\\Supplement_Figure1\\sequence_pairs.png",
-                   width=2400,height=1800,res=450)  
+                   width=2800,height=1800,res=450)  
 total_file %>% 
   filter(!Total_pairs < 5000000 | Total_pairs==NaN) %>% 
   ggplot(aes(x=factor(Cancer_Type,levels = c("Mammary_Cancer","Melanoma", "Osteosarcoma","Lymphoma","Unclassified")),
@@ -55,7 +55,7 @@ dev.off()
 
 ### Unique Mapping Rates ###
 plot_result <- png("G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer_mapping_result\\Supplement_Figure1\\unique-mapping_pairs.png",
-                   width=2400,height=1800,res=450)
+                   width=2800,height=1800,res=450)
 total_file %>% 
   filter(!Total_pairs < 5000000 | Total_pairs==NaN) %>% 
   ggplot(aes(x=factor(Cancer_Type,levels = c("Mammary_Cancer","Melanoma", "Osteosarcoma","Lymphoma","Unclassified")),
@@ -96,7 +96,7 @@ dev.off()
 ###### gt 30 #####
 
 plot_result <- png("G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer_mapping_result\\Supplement_Figure1\\gt-30.png",
-                   width=2400,height=1800,res=450)
+                   width=2800,height=1800,res=450)
 total_file %>% 
   filter(!Total_pairs < 5000000 | Total_pairs==NaN)  %>% 
   filter(!gt_30_fraction < 0.25 |gt_30_fraction ==NaN) %>% 
@@ -140,7 +140,7 @@ dev.off()
 
 #### Unique CDS ####
 plot_result <- png("G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer_mapping_result\\Supplement_Figure1\\unique-CDS-mapping.png",
-                   width=2400,height=1800,res=450)
+                   width=2800,height=1800,res=450)
 total_file %>% 
   filter(!Total_pairs < 5000000 | Total_pairs==NaN) %>%
   filter(as.numeric(uniq_CDS_region_paris_rates) >=0.3)%>% 
@@ -183,7 +183,7 @@ dev.off()
 
 ### change the color of the point ###
 plot_result <- png("G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer_mapping_result\\Supplement_Figure1\\callable-bases.png",
-                   width=2400,height=1800,res=450)
+                   width=2800,height=1800,res=450)
 exclude <- read_excel("G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer_mapping_result\\Supplement_Figure1\\V2Supp1_Data.xlsx",
                       sheet = "Total_excluded")
 callable %>% 
@@ -225,9 +225,55 @@ callable %>%
 #scale_shape_manual(values = 20)
 dev.off()
 
+### Mean Coverage ###
+plot_result <- png("G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer_mapping_result\\Supplement_Figure1\\coverage-Mean.png",
+                   width=2800,height=1800,res=450)
+total_file %>% 
+  filter(!Total_pairs < 5000000 | Total_pairs==NaN) %>%
+  filter(as.numeric(uniq_CDS_region_paris_rates) >=0.3)%>%
+  filter(mean>10) %>%
+  ggplot(aes(x=factor(Cancer_Type,levels = c("Mammary_Cancer","Melanoma", "Osteosarcoma","Lymphoma","Unclassified")),
+             y=as.numeric(mean),fill=Status,color=Status)) +
+  geom_point(size=0.01,position = position_jitterdodge(jitter.width = 0.2)) +
+  
+  ylab("Mean Coverage")+
+  #labs(subtitle = "p<0.01")+
+  #stat_compare_means(aes(group=gene),label="p.signif",symnum.args = symnumargs,label.y = c(2.1,2.1,2.1,2.1,3,3)) +
+  #scale_y_log10(limits=c(0.001,1000),breaks=c(0.001,0.01,0.1,1,10,100,1000),labels=c(0,0.01,0.1,1,10,100,1000)) +
+  theme(axis.line = element_line(colour = "black"),
+        #panel.grid.major = element_blank(),
+        #panel.grid.minor = element_blank(),
+        legend.position = "none",
+        legend.title = element_blank(),
+        legend.key=element_blank(),
+        #legend.text =element_text(color = c("firebrick","black")),
+        legend.background = element_rect(fill = "transparent"),
+        panel.border = element_blank(),
+        axis.title.x = element_blank(),
+        #axis.ticks.x = element_blank(),
+        axis.title.y = element_text(colour="black",size=18,margin = margin(1,5,.5,0)),
+        text = element_text(colour="black",size=14),
+        axis.text.x = element_blank(), 
+        #axis.text.x = element_text(colour=c("black"),size=12,vjust=1,hjust = 0.9),
+        axis.text.y = element_text(colour="black",size=18),
+        panel.background = element_blank())+
+  stat_summary(fun.y = median, fun.ymin = median, fun.ymax = median,position = "dodge",
+               geom = "crossbar",size=0.1, width = .8,colour = "black")+
+  #scale_x_discrete(labels=panel2label)+
+  scale_color_manual(values = c("firebrick","darkolivegreen"))+
+  scale_fill_manual(values=c("firebrick","darkolivegreen"))+
+  scale_shape_manual(values = 20)+
+  theme(plot.margin = unit(c(0.5,0.3,1,0.5), "cm"))
+  #coord_cartesian(ylim=c(0.2,0.8))
+
+
+dev.off()
+
+
+
 #### RMSE ####
 plot_result <- png("G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer_mapping_result\\Supplement_Figure1\\Coverage-RMSE.png",
-                   width=2400,height=1800,res=450)
+                   width=2800,height=1800,res=450)
 total_file %>% 
   filter(!Total_pairs < 5000000 | Total_pairs==NaN) %>%
   filter(as.numeric(uniq_CDS_region_paris_rates) >=0.3)%>% 
