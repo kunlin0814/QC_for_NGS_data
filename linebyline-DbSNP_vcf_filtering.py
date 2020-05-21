0fbacd265261304d61a5578cc3a6c2238c79f563
kunlin0814 <abc730814@gmail.com>
2020-05-20 20:21:43 -0400

    Create linebyline-DbSNP_vcf_filtering.py

---
+++ linebyline-DbSNP_vcf_filtering.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+### python DbSNP_vcf_filtering.py *.vcf <dbSNP.vcf>
+
+import sys
+import glob
+
+def dbsnp_dic(dbsnp):
+    result = {}
+    with open(dbsnp,'r') as f:
+        for lst in f:
+            if lst[0] != '#':
+                info = lst.split('\t')
+                chrom = info[0]
+                pos = info[1]
+                ref = info[3]
+                alt = info[4]
+                Key = chrom+":"+pos
+                if Key not in result.keys():
+                    result[Key] = [ref+":"+alt]
+
+    return result
+
+
+file_name = sys.argv[1]
+dbsnp = sys.argv[2]
+# dbsnp dic
+dbsnpDic = dbsnp_dic(dbsnp)
+# filter vcf files
+with open(file_name,'r') as f:
+    file = f.read()
+
+out = open(file_name.split('.vcf')[0] + 'Line_to_line_tuple_dbSNPfiltered.vcf', 'w')
+lst = file.split('\n')[:-1]
+for i in range(len(lst)):
+    if lst[i][0] == '#':
+        out.write(lst[i] + '\n')
+    else:
+        info = lst[i].split('\t')
+        chrom = info[0]
+        pos = info[1]
+        ref = info[3]
+        alt = info[4]
+        Key = chrom+":"+pos
+        #status = info[6]
+        if Key not in dbsnpDic.keys():
+            out.write(lst[i] + '\n')
+        else:
+            if dbsnpDic[Key] != [ref+":"+alt]:
+                out.write(lst[i] + '\n')
+
+       
+out.close()
+
