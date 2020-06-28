0f4b06344a157e873f71581491a26b60593527ec
kunlin0814 <abc730814@gmail.com>
2020-06-27 22:09:34 -0400

    Update Plotting_WES_QC.R

--- Plotting_WES_QC.R
+++ Plotting_WES_QC.R
@@ -22,29 +22,70 @@ library(RColorBrewer)
 exclude <- read_excel("G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer-analysis\\Figure1\\Original_Data_summary.xlsx",
                       sheet ="Before_Matching_excluded")
 
-total_file <- read_excel("G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer-analysis\\Figure1\\Methods_legends_tables\\Ploting_table.xlsx",
-                          sheet ='NGS_Data_summary', skip =0)
+total_file <- read_excel("G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer-analysis\\Pan-Cancer-Manuscript\\Methods_legends_tables\\Combined_Summary_Public_Data.xlsx",
+                          sheet ='NGS_Data_summary', skip =3)
+
+Combined_total_file <- read_excel("G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer-analysis\\Pan-Cancer-Manuscript\\Combined_Summary_Public_Data.xlsx",
+                         sheet ='NGS_Data_summary', skip =0)
+
+Mut_rate <- read_excel("G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer-analysis\\Mutation_rate\\final_mut_rate.xlsx",
+                       sheet ='Sheet1')
+
+
 
 #plot_result <- png("G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer_mapping_result\\Supplement_Figure1\\sequence_pairs.png",
 #                   width=2800,height=1800,res=450)
-regular.text <- element_text(colour="black",size=26);
+
+
+
+# Mut_rate %>% 
+#   ggplot(aes(x=factor(Cancer_type,levels = c("MC","GLM","LYM","OM", "OSA","HSA","Unclassified")),
+#              y=as.numeric(Mutation_rate),fill=status,color=status)) +
+#   
+#   geom_point(size=1.6,shape=20,position = position_jitterdodge(jitter.width = 0.28)) +
+#   ylab("Mutation Rates")+
+#   #labs(subtitle = "p<0.01")+
+#   #stat_compare_means(aes(group=gene),label="p.signif",symnum.args = symnumargs,label.y = c(2.1,2.1,2.1,2.1,3,3)) +
+#   #scale_y_log10(limits=c(0.001,1000),breaks=c(0.001,0.01,0.1,1,10,100,1000),labels=c(0,0.01,0.1,1,10,100,1000)) +
+#   theme(axis.text=regular.text, 
+#         axis.title.y=regular.text,
+#         axis.title.x =element_blank(),
+#         axis.text.x = element_text(angle=0, hjust=0.5), 
+#         panel.background=element_blank(), 
+#         axis.line=element_line(color="black"),
+#         legend.title=regular.text, 
+#         legend.position="top", 
+#         legend.text=regular.text, 
+#         legend.key=element_blank())+
+#   stat_summary(fun.y = median, fun.ymin = median, fun.ymax = median,position = "dodge",
+#                geom = "crossbar",size=0.5, width = .7,colour = "black")+
+#   #scale_x_discrete(labels=panel2label)+
+#   scale_color_manual(values = c("darkblue","red3"))+
+#   scale_fill_manual(values=c("darkblue","red3"))+
+#   #scale_fill_manual(values=c("firebrick","darkolivegreen"))+
+#   #scale_shape_manual(values = 20)+
+#   theme(plot.margin = unit(c(1,0.3,1.5,0.5), "cm"))+
+#   coord_cartesian(ylim=c(0,100))
+#  
+
+
 
 
 pdf("G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer-analysis\\Figure1\\F1_and_supplementaryF1.pdf"
 , height=4.8, width=6.2);
+regular.text <- element_text(colour="black",size=20);
 
-
-
-tiff(file = "G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer-analysis\\Figure1\\square-sequence_pairs.tiff", 
-     width = 3500, height =3000, units = "px", res = 400)
+# # 
+# tiff(file = "G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer-analysis\\Figure1\\square-sequence_pairs.tiff",
+#      width = 3500, height =3000, units = "px", res = 400)
 
 
 total_file %>% 
   #filter(!Total_pairs < 5000000 | Total_pairs==NaN) %>% 
-  ggplot(aes(x=factor(Cancer_Type,levels = c("MC","GLM","BCL","TCL","OM", "OSA","HSA","Unclassified")),
+  ggplot(aes(x=factor(Cancer_Type,levels = c("MC","GLM","LYM","OM", "OSA","HSA","Unclassified")),
              y=as.numeric(Total_pairs)/1000000,fill=Status,color=Status)) +
   
-  geom_point(size=3,shape=20,position = position_jitterdodge(jitter.width = 0.28)) +
+  geom_point(size=1.6,shape=20,position = position_jitterdodge(jitter.width = 0.28)) +
   ylab("Sequence Read Pairs in Millions")+
   scale_y_continuous(breaks = c(0,100,200))+
   #labs(subtitle = "p<0.01")+
@@ -69,24 +110,23 @@ total_file %>%
   #scale_shape_manual(values = 20)+
   coord_cartesian(ylim=c(0,200))+
   theme(plot.margin = unit(c(1,0.3,1.5,0.5), "cm"))+
-  geom_hline(yintercept=5, linetype="longdash", color = "yellow4", size = 1)
+  geom_hline(yintercept=5, linetype="longdash", color = "yellow4", size = 0.7)
  
- dev.off()
+ # dev.off()
 
 ### Unique Mapping Rates ###
 #plot_result <- png("G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer_mapping_result\\Supplement_Figure1\\unique-mapping_pairs.png",
 #                   width=2800,height=1800,res=350)
 # 
-# tiff(file = "G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer-analysis\\Figure1\\unique-mapping_pairs.tiff", 
-#      width = 3500, height =1900, units = "px", res = 400)
-tiff(file = "G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer-analysis\\Figure1\\unique-mapping_pairs.tiff", 
-    width = 3500, height =3000, units = "px", res = 400)
+
+# tiff(file = "G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer-analysis\\Figure1\\unique-mapping_pairs.tiff",
+#     width = 3500, height =3000, units = "px", res = 400)
 
 total_file %>% 
   filter(!Total_pairs < 5000000 | Total_pairs==NaN) %>% 
-  ggplot(aes(x=factor(Cancer_Type,levels = c("MC","GLM","BCL","TCL","OM", "OSA","HSA","Unclassified")),
+  ggplot(aes(x=factor(Cancer_Type,levels = c("MC","GLM","LYM","OM", "OSA","HSA","Unclassified")),
              y=as.numeric(Uniquely_mapped_rate),fill=Status,color=Status)) +
-  geom_point(size=3,shape=20,position = position_jitterdodge(jitter.width = 0.28)) +
+  geom_point(size=1.6,shape=20,position = position_jitterdodge(jitter.width = 0.28)) +
   ylab("Uniquely Mapping Rate")+
   scale_y_continuous(breaks = c(0,0.5,0.75,1.0))+
   #labs(subtitle = "p<0.01")+
@@ -111,21 +151,21 @@ total_file %>%
   #scale_shape_manual(values = 19)+
   coord_cartesian(ylim=c(0.3,1))+
   theme(plot.margin = unit(c(1,0.3,1.5,0.5), "cm"))+
-  geom_hline(yintercept=0.6, linetype="longdash", color = "yellow4", size = 1)
-  
+  geom_hline(yintercept=0.6, linetype="longdash", color = "yellow4", size = 0.7)
 
-dev.off()
+
+# dev.off()
 ###### gt 30 #####
 
-tiff(file = "G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer-analysis\\Figure1\\gt-30.tiff", 
-      width = 3500, height =3000, units = "px", res = 400)
+# tiff(file = "G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer-analysis\\Figure1\\gt-30.tiff",
+#       width = 3500, height =3000, units = "px", res = 400)
 
 total_file %>% 
   filter(!Total_pairs < 5000000 | Total_pairs==NaN)  %>% 
   #filter(!gt_30_fraction < 0.25 |gt_30_fraction ==NaN) %>% 
-  ggplot(aes(x=factor(Cancer_Type,levels = c("MC","GLM","BCL","TCL","OM", "OSA","HSA","Unclassified")),
+  ggplot(aes(x=factor(Cancer_Type,levels = c("MC","GLM","LYM","OM", "OSA","HSA","Unclassified")),
              y=as.numeric(gt_30_fraction),fill=Status,color=Status)) +
-  geom_point(size=3,shape=20,position = position_jitterdodge(jitter.width = 0.28)) +
+  geom_point(size=1.6,shape=20,position = position_jitterdodge(jitter.width = 0.28)) +
   ylab("Fraction of mapping quality >30")+
   scale_y_continuous(breaks = c(0,0.5,0.75,1.0))+
   #labs(subtitle = "p<0.01")+
@@ -152,18 +192,18 @@ total_file %>%
   theme(plot.margin = unit(c(1,0.3,1.5,0.5), "cm"))
 
 
-dev.off()
+# dev.off()
 
 
 
 #### Unique CDS ####
-# tiff(file = "G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer-analysis\\Figure1\\unique-CDS-mapping.tiff", 
+# tiff(file = "G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer-analysis\\Figure1\\unique-CDS-mapping.tiff",
 #      width = 3500, height =1900, units = "px", res = 400)
 
 total_file %>% 
   filter(!Total_pairs < 5000000 | Total_pairs==NaN) %>%
   filter(as.numeric(Uniquely_mapped_rate) >=0.6)%>% 
-  ggplot(aes(x=factor(Cancer_Type,levels = c("MC","GLM","BCL","TCL","OM", "OSA","HSA","Unclassified")),
+  ggplot(aes(x=factor(Cancer_Type,levels = c("MC","GLM","LYM","OM", "OSA","HSA","Unclassified")),
              y=as.numeric(uniq_CDS_region_paris_rates),fill=Status,color=Status)) +
   geom_point(size=1.6,shape=20,position = position_jitterdodge(jitter.width = 0.28)) +
   
@@ -205,7 +245,7 @@ total_file %>%
   filter(as.numeric(Uniquely_mapped_rate) >=0.6) %>% 
   filter(as.numeric(uniq_CDS_region_paris_rates) >=0.3)%>%
   #filter(mean >30) %>% 
-  ggplot(aes(x=factor(Cancer_Type,levels = c("MC","GLM","BCL","TCL","OM", "OSA","HSA","Unclassified")),
+  ggplot(aes(x=factor(Cancer_Type,levels = c("MC","GLM","LYM","OM", "OSA","HSA","Unclassified")),
              y=as.numeric(mean),fill=Status,color=Status)) +
   geom_point(size=1.6,shape=20,position = position_jitterdodge(jitter.width = 0.28)) +
   scale_y_continuous(breaks = c(0,100,200))+
@@ -249,7 +289,7 @@ total_file %>%
   filter(as.numeric(Uniquely_mapped_rate) >=0.6) %>% 
   filter(as.numeric(uniq_CDS_region_paris_rates) >=0.3)%>% 
   filter(mean>30) %>% 
-  ggplot(aes(x=factor(Cancer_Type,levels = c("MC","GLM","BCL","TCL","OM", "OSA","HSA","Unclassified")),
+  ggplot(aes(x=factor(Cancer_Type,levels = c("MC","GLM","LYM","OM", "OSA","HSA","Unclassified")),
              y=as.numeric(RMSE),fill=Status,color=Status)) +
   geom_point(size=1.6,shape=20,position = position_jitterdodge(jitter.width = 0.28)) +
   scale_y_continuous(breaks = c(0,0.005,0.01))+
@@ -278,21 +318,23 @@ total_file %>%
   theme(plot.margin = unit(c(1,0.3,1.5,0.5), "cm"))
 
 
-# dev.off()
+dev.off()
 
 
 ### change the color of the point ###
 # tiff(file = "G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer-analysis\\Figure1\\callable-bases.tiff", 
 #      width = 3500, height =1900, units = "px", res = 400)
-pdf("G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer-analysis\\Figure1\\callablebases.pdf"
-    , height=3.6, width=6.2);
+# pdf("G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer-analysis\\Figure1\\callablebases.pdf"
+#     , height=3.6, width=6.2);
 
+pdf("G:\\MAC_Research_Data\\Pan_cancer\\Pan_cancer-analysis\\Figure1\\callable_bases.pdf"
+    , height=3.6, width=6.2);
 
 total_file %>% 
   filter(Status=='Tumor') %>% 
   filter(!Case_ID %in% exclude$Cases) %>% 
   filter(!Callable_bases =='No-Pair')%>% 
-  ggplot(aes(x=factor(Cancer_Type,levels = c("MC","GLM","BCL","TCL","OM", "OSA","HSA","Unclassified")),
+  ggplot(aes(x=factor(Cancer_Type,levels = c("MC","GLM","LYM","OM", "OSA","HSA","Unclassified")),
              y=as.numeric(Callable_bases)/1000000,color='black')) +
   geom_point(size=1.6,shape=20,position = position_jitterdodge(jitter.width = 0.28)) +
   ylab("Callable bases in Millions")+
