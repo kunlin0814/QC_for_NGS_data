e3a56da6ab4bfc81602e3d54e1dc6f459608c487
kunlin0814 <abc730814@gmail.com>
2020-12-04 22:27:46 -0500

    Update i_FECA_Summarize_BWA_WGS.sh

--- i_FECA_Summarize_BWA_WGS.sh
+++ i_FECA_Summarize_BWA_WGS.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1                   # Run all processes on a single node
 #SBATCH --ntasks=1                  # Run in a single task on a single node
 #SBATCH --cpus-per-task=1           # Number of CPU cores per task (4)
-#SBATCH --mem=40G                   # Job memory limit (10 GB)
+#SBATCH --mem=50G                   # Job memory limit (10 GB)
 #SBATCH --time=40:00:00              # Time limit hrs:min:sec or days-hours:minutes:seconds
 #SBATCH --output=i_FECA_Summarized_BWA_WGS.%j.out    # Standard output log
 #SBATCH --error=i_FECA_Summarized_BWA_WGS.%j.err     # Standard error log
