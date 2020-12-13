316e19120e0781cf00bf5992904463f86ad46d25
kunlin0814 <abc730814@gmail.com>
2020-12-13 13:02:47 -0500

    Update FinalWesQcPipeline.sh

--- FinalWesQcPipeline.sh
+++ FinalWesQcPipeline.sh
@@ -17,7 +17,7 @@ Sequence_length=101
 sample_name='CMT-2'
 source_folder='/scratch/kh31516/Original_Mammary/results/PRJNA489159/'${sample_name} #
 script='/home/kh31516/kh31516_Lab_Share_script'
-file_output='/scratch/kh31516/Original_Mammary/results/PRJNA489159/QC/'${sample_name}
+sam_file_output='/scratch/kh31516/Original_Mammary/results/PRJNA489159/QC/'${sample_name}
 summary_output='/scratch/kh31516/Original_Mammary/results/PRJNA489159/'
 DepthOfCoverage='/scratch/kh31516/Original_Mammary/results/PRJNA489159/DepthOfCoverage/'${sample_name}
 Mutect='/scratch/kh31516/Original_Mammary/results/PRJNA489159/Mutect/'${sample_name}
@@ -30,41 +30,41 @@ module load SAMtools/1.9-GCC-8.3.0
 ml Anaconda3/2020.02
 #source activate py35
 
-mkdir -p $file_output
+mkdir -p ${sam_file_output}
 ## Convert the bam file into sam file
 cd ${source_folder}/
 
-samtools view $source_folder/${Normal_Sample}.bam > $sam_file_output/${Normal_Sample}.sam # normal
-samtools view $source_folder/${Tumor_Sample}.bam > $sam_file_output/${Tumor_Sample}.sam # tumor
+samtools view $source_folder/${Normal_Sample}.bam > ${sam_file_output}/${Normal_Sample}.sam # normal
+samtools view $source_folder/${Tumor_Sample}.bam > ${sam_file_output}/${Tumor_Sample}.sam # tumor
 
 ###### Analyze the BWA mapping and CDS region mapping result ######
 
-cd ${file_output}/
+cd ${sam_file_output}/
 
 java -cp ${script}/ Line_by_line_Total_dict_Get_exon_reads \
-${file_output}/${Normal_Sample}.sam \
+${sam_file_output}/${Normal_Sample}.sam \
 ${Normal_Sample} \
-${file_output}/Normal-${Normal_Sample}-CDS_Mapping_summary.txt \
+${sam_file_output}/Normal-${Normal_Sample}-CDS_Mapping_summary.txt \
 $Sequence_length \
 ${Cancer_Type} \
 "Normal"
 
 java -cp ${script}/ Line_by_line_Total_dict_Get_exon_reads \
-${file_output}/${Tumor_Sample}.sam \
+${sam_file_output}/${Tumor_Sample}.sam \
 ${Tumor_Sample} \
-${file_output}/Tumor-${Tumor_Sample}-CDS_Mapping_summary.txt \
+${sam_file_output}/Tumor-${Tumor_Sample}-CDS_Mapping_summary.txt \
 $Sequence_length \
 ${Cancer_Type} \
 "Tumor"
 
-cat ${file_output}/Normal-${Normal_Sample}-CDS_Mapping_summary.txt >> $summary_output/Total_WES_BWA_CDS_${Bioproject}_${Cancer_Type}.txt
-cat ${file_output}/Tumor-${Tumor_Sample}-CDS_Mapping_summary.txt >>  $summary_output/Total_WES_BWA_CDS_${Bioproject}_${Cancer_Type}.txt
+cat ${sam_file_output}/Normal-${Normal_Sample}-CDS_Mapping_summary.txt >> $summary_output/Total_WES_BWA_CDS_${Bioproject}_${Cancer_Type}.txt
+cat ${sam_file_output}/Tumor-${Tumor_Sample}-CDS_Mapping_summary.txt >>  $summary_output/Total_WES_BWA_CDS_${Bioproject}_${Cancer_Type}.txt
 
 ##### Sequence Reads Mapping Quality #####
 
-cd ${file_output}/
+cd ${sam_file_output}/
 
-cat ${file_output}/${Normal_Sample}.sam | cut -f5 > ${file_output}/${Normal_Sample}-mapping_quality
+cat ${sam_file_output}/${Normal_Sample}.sam | cut -f5 > ${sam_file_output}/${Normal_Sample}-mapping_quality
 
 Ngt30=$(cat ${Normal_Sample}-mapping_quality | awk '{if ($1>=30) {print $1}}' | wc -l)
 Ngt60=$(cat ${Normal_Sample}-mapping_quality | awk '{if ($1>=60) {print $1}}' | wc -l)
@@ -75,11 +75,11 @@ Nfra60=$(echo "$((Ngt60))/$((Ntotal))" | bc -l)
 printf  "%s\t%4f\t%4f\t%s\t%s\n" "${Normal_Sample}" "${Nfra30}" "${Nfra60}" "${Cancer_Type}" "Normal" >> \
 $summary_output/WES_Total_Mapping_quality_${Bioproject}_${Cancer_Type}.txt
 
-cat ${file_output}/${Tumor_Sample}.sam | cut -f5 > ${file_output}/${Tumor_Sample}-mapping_quality
+cat ${sam_file_output}/${Tumor_Sample}.sam | cut -f5 > ${sam_file_output}/${Tumor_Sample}-mapping_quality
 
-Tgt30=$(cat ${file_output}/${Tumor_Sample}-mapping_quality | awk '{if ($1>=30) {print $1}}' | wc -l)
-Tgt60=$(cat ${file_output}/${Tumor_Sample}-mapping_quality | awk '{if ($1>=60) {print $1}}' | wc -l)
-Ttotal=$(cat ${file_output}/${Tumor_Sample}-mapping_quality| wc -l)
+Tgt30=$(cat ${sam_file_output}/${Tumor_Sample}-mapping_quality | awk '{if ($1>=30) {print $1}}' | wc -l)
+Tgt60=$(cat ${sam_file_output}/${Tumor_Sample}-mapping_quality | awk '{if ($1>=60) {print $1}}' | wc -l)
+Ttotal=$(cat ${sam_file_output}/${Tumor_Sample}-mapping_quality| wc -l)
 Tfra30=$(echo "$((Tgt30))/$((Ttotal))" | bc -l)
 Tfra60=$(echo "$((Tgt60))/$((Ttotal))" | bc -l)
 
@@ -109,20 +109,20 @@ cat ${DepthOfCoverage}/${Tumor_Sample}_randomness_summary.txt >> $summary_output
 reference='/work/szlab/Lab_shared_PanCancer/source'
 #Yuan_script='/work/szlab/Lab_shared_PanCancer/script'
 
-cd ${file_output}
+cd ${sam_file_output}
 
 callable=$(cat $Mutect/${sample_name}_rg_added_sorted_dedupped_removed.bam_coverage.wig.txt | grep "^1"| wc -l)
 
 printf  "%s\t%d\t%s\t%\%s\n" "${sample_name}" "${callable}" "${Cancer_Type}" "${Bioproject}" >> $summary_output/Total_WES_Callable_${Bioproject}_${Cancer_Type}.txt
 
 : "
-rm ${file_output}/${Normal_Sample}.sam
-rm ${file_output}/${Tumor_Sample}.sam
-rm ${file_output}/${Normal_Sample}_BWA_summarize
-rm ${file_output}/${Tumor_Sample}_BWA_summarize
-rm ${file_output}/Normal-${Normal_Sample}-CDS_Mapping_summary.txt
-rm ${file_output}/Tumor-${Tumor_Sample}-CDS_Mapping_summary.txt
-rm ${file_output}/${Normal_Sample}-mapping_quality ${file_output}/${Tumor_Sample}-mapping_quality
+rm ${sam_file_output}/${Normal_Sample}.sam
+rm ${sam_file_output}/${Tumor_Sample}.sam
+rm ${sam_file_output}/${Normal_Sample}_BWA_summarize
+rm ${sam_file_output}/${Tumor_Sample}_BWA_summarize
+rm ${sam_file_output}/Normal-${Normal_Sample}-CDS_Mapping_summary.txt
+rm ${sam_file_output}/Tumor-${Tumor_Sample}-CDS_Mapping_summary.txt
+rm ${sam_file_output}/${Normal_Sample}-mapping_quality ${sam_file_output}/${Tumor_Sample}-mapping_quality
 rm ${DepthOfCoverage}/${Normal_Sample}_DepthofCoverage_Distribution.txt
 rm ${DepthOfCoverage}/${Tumor_Sample}_DepthofCoverage_Distribution.txt
 rm ${DepthOfCoverage}/${Tumor_Sample}_randomness_summary.txt
